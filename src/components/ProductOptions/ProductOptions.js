c444cbcfc63e39ea396d97fe8be3db71a2810c93
Maciej Laskowski <maciek30088@gmail.com>
2023-05-28 22:46:45 +0100

    Use hook useMemo in calculatePrice function

--- src/components/ProductOptions/ProductOptions.js
+++ src/components/ProductOptions/ProductOptions.js
@@ -1,31 +1,41 @@
+import { useMemo } from 'react';
+import PropTypes from 'prop-types';
 import styles from './ProductOptions.module.scss';
 import Button from '../Button/Button';
 import OptionColor from './OptionColor/OptionColor';
 import OptionSize from './OptionSize/OptionSize.js';
-import PropTypes from 'prop-types';
+
 const ProductOptions = (props) => {
+  const calculatePrice = useMemo(() => {
+    let price = props.basePrice;
+    if (props.currentSize.additionalPrice) {
+      price += props.currentSize.additionalPrice;
+    }
+    return price;
+  }, [props.basePrice, props.currentSize]);
 
   const handleAddToCart = (event) => {
     event.preventDefault();
     console.log('Product Summary:');
     console.log('=====================');
     console.log('Name:', props.title);
-    console.log('Final Price:', calculatePrice());
+    console.log('Final Price:', calculatePrice);
     console.log('Selected Size:', props.currentSize.name);
     console.log('Selected Color:', props.currentColor);
-  };
-  const calculatePrice = () => {
-    let price = props.basePrice;
-    if (props.currentSize.additionalPrice) {
-      price += props.currentSize.additionalPrice;
-    }
-    return price;
   };
 
   return (
     <form>
-      <OptionSize sizes={props.sizes} currentSize={props.currentSize} setCurrentSize={props.setCurrentSize} />
-      <OptionColor colors={props.colors} currentColor={props.currentColor} setCurrentColor={props.setCurrentColor} />
+      <OptionSize
+        sizes={props.sizes}
+        currentSize={props.currentSize}
+        setCurrentSize={props.setCurrentSize}
+      />
+      <OptionColor
+        colors={props.colors}
+        currentColor={props.currentColor}
+        setCurrentColor={props.setCurrentColor}
+      />
       <Button className={styles.button} onClick={handleAddToCart}>
         <span className="fa fa-shopping-cart" />
       </Button>
